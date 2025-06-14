b39c50ca777adf9a38b66989214f4b19ddba74eb
Younesbih <younesbih6@gmail.com>
2025-06-14 19:09:14 +0100

    Aui

--- bubble_sort_algo.py
+++ bubble_sort_algo.py
@@ -1,6 +1,5 @@
 # this algo is about to sort in a descending way fixing element by element
-def Sorting():
-    numbers = [9,4,6,7,1,5,0,3,2]
+def Sorting(numbers):
     for j in range(len(numbers)-1):
         for i in range(len(numbers)-1-j):
             if numbers[i]> numbers[i+1]:
@@ -9,5 +8,5 @@ def Sorting():
                 numbers[i+1] = temp
                 print(numbers)
     return numbers
-result = Sorting()
+result = Sorting([9,4,6,7,1,5,0,3,2])
 print(result)
